4f11ef40f69e04052729dc387bf04d19a442779e
alebautt <id_bautista@hotmail.com>
2015-11-28 00:54:49 -0600

    Phone login

---
+++ PhoneRegister/Register.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  PhoneRegister
+//
+//  Created by Alejandra B on 27/11/15.
+//  Copyright © 2015 alebautista. All rights reserved.
+//
+
+#import "Register.h"
+#import <DigitsKit/DigitsKit.h>
+
+
+@interface Register ()
+
+@end
+
+@implementation Register
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    DGTAuthenticateButton *authButton;
+    authButton = [DGTAuthenticateButton buttonWithAuthenticationCompletion:^(DGTSession *session, NSError *error) {
+        if (session.userID) {
+            // TODO: associate the session userID with your user model
+            NSString *msg = [NSString stringWithFormat:@"Phone number: %@", session.phoneNumber];
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"You are logged in!"
+                                                            message:msg
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+        } else if (error) {
+            NSLog(@"Authentication error: %@", error.localizedDescription);
+        }
+    }];
+    authButton.center = self.view.center;
+    [self.view addSubview:authButton];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
