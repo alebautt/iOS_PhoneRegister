475c5953852e866964cdfbde3ec4ececd2dd06d9
alebautt <id_bautista@hotmail.com>
2015-11-27 21:43:38 -0600

    Initial Commit

---
+++ PhoneRegister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhoneRegister
+//
+//  Created by Alejandra B on 27/11/15.
+//  Copyright © 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhoneRegister/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhoneRegister
+//
+//  Created by Alejandra B on 27/11/15.
+//  Copyright © 2015 alebautista. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
